732a47a427e6f0c622bd69770eaa01cf985569f8
Travis Matthews <tmatthew@e1z3r7p25.42.us.org>
2019-08-02 13:42:14 -0700

    created index.html, added HtmlWebpackPlugin to generate & place in dist

--- client/build/webpack.config.js
+++ client/build/webpack.config.js
@@ -1,4 +1,13 @@
+'use strict'
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const build = (...dirs) => path.resolve(process.cwd(), ...dirs)
+
 module.exports = {
+	entry: {
+		main: './src/landing.js'
+	},
 	module: {
 		// Define modifications to occur when module is created
 		rules: [
@@ -11,5 +20,12 @@ module.exports = {
 				}
 			}
 		]
-	}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: build('assets', 'index.html'),
+			title: 'index',
+			filename: 'index.html'
+		})
+	]
 }
