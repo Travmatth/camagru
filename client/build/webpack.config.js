d10152fa89d820180eb861463d41dd073e64290c
Travis Matthews <tmatthew@e1z3r7p25.42.us.org>
2019-08-07 15:58:18 -0700

    added scss & uikit

--- client/build/webpack.config.js
+++ client/build/webpack.config.js
@@ -3,30 +3,58 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const build = (...dirs) => path.resolve(process.cwd(), ...dirs)
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 	entry: {
-		main: './src/landing.js'
+		index: [
+			build('src', 'landing'),
+			build('src', 'landing-styles')
+		],
 	},
 	resolve: {
-		extensions: ['.js', '.jsx']
+		extensions: ['.js', '.jsx', '.scss']
 	},
 	module: {
 		rules: [
+			{
+				test: /\.ejs$/,
+				exclude: /node_modules/,
+				use: 'ejs-loader'
+			},
 			{
 				test: /\.(js|jsx)$/,
 				exclude: /node_modules/,
 				use: {
 					loader: "babel-loader"
 				}
+			},
+			{
+				test: /\.scss$/,
+				use:[
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'sass-loader'
+				],
+			},
+			{
+				test: /\.(ico|jpe?g|png|gif)$/,
+				loader: 'file?name=[path][name].[ext]'
+			},
+			{
+				test: /\.(woff|woff2|ttf|otf|eot\?#.+|svg#.+)$/,
+				loader: "file?name=[path][name].[ext]"
 			}
 		]
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: build('assets', 'index.html'),
+			template: build('assets', 'index.ejs'),
 			title: 'index',
 			filename: 'index.html'
+		}),
+		new MiniCssExtractPlugin({
+			filename: "[name].css"
 		})
 	],
 	devServer: {
--- client/src/landing.js
+++ client/src/landing.js
@@ -1,9 +1,17 @@
+import UIkit from 'uikit'
+import Icons from 'uikit/dist/js/uikit-icons'
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = 'React with Webpack and Babel';
+UIkit.use(Icons)
+UIkit.notification('hello world')
 
 ReactDOM.render(
-  <div>{title}</div>,
+    <div class="uk-container">
+        <div class="uk-card uk-card-body uk-card-primary">
+            <h3 class="uk-card-title">Hello World</h3>
+            <button class="uk-button uk-button-default" uk-tooltip="title: Hover Working">Hover</button>
+        </div>
+    </div>,
   document.getElementById('app')
 );
