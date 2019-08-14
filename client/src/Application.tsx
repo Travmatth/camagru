8402c2dccda6c525ee7e7d7c5ee3160572efc352
Travis Matthews <trav.matth@gmail.com>
2019-08-14 13:31:09 -0700

    basic react router

--- client/src/Application.tsx
+++ client/src/Application.tsx
@@ -1,16 +1,50 @@
 import * as React from 'react';
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import * as UIkit from 'uikit';
 import * as Icons from 'uikit/dist/js/uikit-icons';
 import ApiManager from './ApiManager';
 
 const InitUIKit = () => {
     UIkit.use(Icons)
-    UIkit.notification('hello world')
 }
 
 type CamagruProps = {
 	api: ApiManager
 }
+
+const NavBar = () => (
+	<div>
+		<Link to="/">Home</Link>
+		<Link to="/studio">Studio</Link>
+	</div>
+);
+
+const Home = () => (
+	<div className="uk-container">
+		<div className="uk-card uk-card-body uk-card-primary">
+			<h3 className="uk-card-title">Home</h3>
+			<button className="uk-button uk-button-default" uk-tooltip="title: Hover Working">Hover</button>
+		</div>
+	</div>
+);
+
+const Studio = () => (
+	<div className="uk-container">
+		<div className="uk-card uk-card-body uk-card-primary">
+			<h3 className="uk-card-title">Studio</h3>
+			<button className="uk-button uk-button-default" uk-tooltip="title: Hover Working">Hover</button>
+		</div>
+	</div>
+);
+
+const NotFound = () => (
+	<div className="uk-container">
+		<div className="uk-card uk-card-body uk-card-primary">
+			<h3 className="uk-card-title">NotFound</h3>
+			<button className="uk-button uk-button-default" uk-tooltip="title: Hover Working">Hover</button>
+		</div>
+	</div>
+);
 
 class Application extends React.Component<CamagruProps , any> {
 	constructor(props: any) {
@@ -20,12 +54,14 @@ class Application extends React.Component<CamagruProps , any> {
 	render() {
 		this.props.api.login();
 		return (
-			<div className="uk-container">
-				<div className="uk-card uk-card-body uk-card-primary">
-					<h3 className="uk-card-title">Hello World</h3>
-					<button className="uk-button uk-button-default" uk-tooltip="title: Hover Working">Hover</button>
-				</div>
-			</div>
+			<BrowserRouter>
+				<NavBar/>
+				<Switch>
+					<Route exact path="/" component={Home}/>
+					<Route path="/studio" component={Studio}/>
+					<Route component={NotFound}/>
+				</Switch>
+			</BrowserRouter>
 		)
 	}
 }
