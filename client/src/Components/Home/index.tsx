358eba37ff24553b451109c60dc3999c5b133bab
Travis Matthews <trav.matth@gmail.com>
2019-08-15 21:33:03 -0700

    signup/login button layout

--- client/src/Components/Home/index.tsx
+++ client/src/Components/Home/index.tsx
@@ -1,12 +1,24 @@
 import * as React from 'react';
+import { RouteComponentProps } from "react-router-dom";
+import { ApiProps } from '../../Application';
 
-const Home = () => (
-	<div className="uk-container">
-		<div className="uk-card uk-card-body uk-card-primary">
-			<h3 className="uk-card-title">Home</h3>
-			<button className="uk-button uk-button-default" uk-tooltip="title: Hover Working">Hover</button>
-		</div>
-	</div>
-);
+class Home extends React.Component<ApiProps & RouteComponentProps, any> {
+	render() {
+		return (
+			<div className="uk-container uk-background-primary uk-text-center">
+				<div className="uk-container uk-background-primary uk-width-1-1@s uk-width-1-3@m uk-width-1-4@l">
+					<div className="uk-child-width-1-2" data-uk-grid>
+						<div className="">
+							<button className="uk-button">Sign Up</button>
+						</div>
+						<div className="">
+							<button className="uk-button">Log In</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
 
 export default Home
