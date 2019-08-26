a945c64ecad4e0e07c2113448f3ad5de2925bde8
Travis Matthews <tmatthew@e1z3r7p25.42.us.org>
2019-08-26 14:15:26 -0700

    committing git-crypted files

--- bin/setup_vm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-docker-machine start Camagru
-eval $(docker-machine env Camagru)
-docker-machine ssh Camagru sudo mkdir /client
-docker-machine ssh Camagru sudo mount -t vboxsf client /client
-echo 'docker-machine created, run `eval $(docker-machine env Camagru)` to connect to current shell'
--- docker/client.Dockerfile
+++ docker/client.Dockerfile
@@ -1,4 +1,4 @@
 FROM node:latest
 
 WORKDIR /app
-CMD yarn build
+CMD npm build
